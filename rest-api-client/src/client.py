09dc14e71b520b8f65232681b94878ce12c95898
basyair7 <basyair.fathul@gmail.com>
2024-04-15 23:38:32 +0700

    modified program Client

--- rest-api-client/src/client.py
+++ rest-api-client/src/client.py
@@ -21,9 +21,24 @@ class clientRestAPI:
         except (requests.exceptions.RequestException, requests.ConnectTimeout) as e:
             print("Error: ", e);
             return None;
+        
+class SaveData:
+    def __init__(this, nameFile: str, value):
+        this.namefile = nameFile;
+        this.value = value;
+        try:
+            with open(this.namefile, 'w') as file:
+                if(isinstance(value, list)):
+                    for i in value:
+                        file.write(str(i)+"\n");
+                else:
+                    file.write(str(value));
+                    
+        except Exception as e:
+            print("Error: ", e);
     
     
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     os.system("cls") if (os.name == "nt") else os.system("clear");
     
     user = os.getenv("USER");
@@ -35,16 +50,22 @@ if __name__ == "__main__":
         user = os.getenv("USER");
         password = os.getenv("PASSWORD");
         url_api = os.getenv("URL");
-        
+    
+    filename = "Sensor.txt"
     data = clientRestAPI(f"{url_api}/sensor/sensorid/1", user, password)
     
     if (data.getdata()):
+        value = []
         for (item) in (data.getdata()):
+            value.append(item["value"]);
             print("ID\t\t : ", item["id"]);
             print("Sensor ID\t : ", item["sensorid"]);
             print("Value\t\t : ", item["value"]);
             print("Create Date\t : ", item["createDate"]);
             print("Update Date\t : ", item["updateDate"])
             print();
+                        
+        SaveData(filename, value)
+
     else:
         print("Failed to fetch data from API server")
