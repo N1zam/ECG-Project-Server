4b7ba07c1f694c886cb7eecf84f7f1a19ca9d7ac
basyair7 <basyair.fathul@gmail.com>
2024-05-25 10:13:42 +0700

    add library client

---
+++ rest-api-client/lib/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='clientRestAPI',
+version='0.1.0',
+author='basyair7',
+author_email='basyair.fathul@yahoo.com',
+description='Library berisi get data dari rest api dan simpan data dalam bentuk file',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: GPL-3.0 License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
--- rest-api-client/src/client.py
+++ rest-api-client/src/client.py
@@ -1,58 +1,8 @@
 import requests, os, keyboard, json
+from clientRestAPI import clientRestAPI, File
 from time import sleep
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv, dotenv_values
-
-class clientRestAPI:
-    def __init__(this, api_url, user, password):
-        this.api_url = api_url;
-        this.user = user;
-        this.password = password;
-        
-    def getdata(this):
-        return (this._fetch_data_from_api())
-    
-    def _fetch_data_from_api(this):
-        try:
-            res = requests.get(
-                url=this.api_url, auth=HTTPBasicAuth(this.user, this.password)
-            );
-            data = res.json();
-            return data;
-        except (requests.exceptions.RequestException, requests.ConnectTimeout) as e:
-            print("Error: ", e);
-            return None;
-        
-class File:
-    def __init__(this, nameFile: str):
-        this.namefile = nameFile;
-        
-    def read(this):
-        try:
-            with open(this.namefile, "r", encoding="utf-8") as file:
-                data = [data.strip() for data in file.readlines()]
-                file.close();
-            return data;
-
-        
-        except Exception as e:
-            print("Error: ", e);
-    
-    def save(this, value):
-        try:
-            with open(this.namefile, "w", encoding="utf-8") as file:
-                if(isinstance(value, list)):
-                    for i in value:
-                        file.write(str(i)+"\n");
-                else:
-                    file.write(f"{value:.1f}\n");
-                    
-                file.close();
-                
-        except Exception as e:
-            print("Error: ", e);
-            
-            
 
 def main():    
     user = os.getenv("USER");
